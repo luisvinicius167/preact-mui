44251588502dc4c0ef9626cfbe1545a9e748b4b0
Luis Vinicius <luis@startamus.com.br>
2016-12-08 18:11:13 -0200

    0.0.5

--- dist/preact-mui.js
+++ dist/preact-mui.js
@@ -1,7 +1,5 @@
 'use strict';
-
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
 (function (global, factory) {
   (typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? factory() : typeof define === 'function' && define.amd ? define(factory) : factory();
 })(undefined, function () {
