d2c69fb0900e095a86ff368678aba041c3b29331
Luis Vinicius <luis@startamus.com.br>
2016-12-12 10:16:42 -0200

    add more support col

--- src/components/col/index.js
+++ src/components/col/index.js
@@ -10,12 +10,18 @@ import {h, Component} from 'preact';
  * @name Col
  */ 
 export default class Col extends Component {
-  render( {children, md, mdOffset, xs, xsOffset, ...props} ) {
+  render( {children, xs, xsOffset, sm, smOffset, md, mdOffset, lg, lgOffset, xl, xlOffset, ...props} ) {
     let colMdClass = md ? `mui-col-md-${md}` : ''
       , colXsClass = xs ? `mui-col-xs-${xs}` : ''
-      , mdOffsetClass = mdOffset ? `mui-col-md-offset-${mdOffset}` : ''
+      , colSmClass = sm ? `mui-col-sm-${sm}` : ''
+      , colLgClass = lg ? `mui-col-lg-${lg}` : ''
+      , colXlClass = xl ? `mui-col-xs-${xl}` : ''
       , xsOffsetClass = xsOffset ? `mui-col-xs-offset-${xsOffset}` : ''
-      , className = `${colMdClass} ${colXsClass} ${mdOffsetClass} ${xsOffsetClass}`.trim();
+      , smOffsetClass = smOffset ? `mui-col-sm-offset-${smOffset}` : ''
+      , mdOffsetClass = mdOffset ? `mui-col-md-offset-${mdOffset}` : ''
+      , lgOffsetClass = lgOffset ? `mui-col-lg-offset-${lgOffset}` : ''
+      , xlOffsetClass = xlOffset ? `mui-col-xl-offset-${xlOffset}` : ''
+      , className = `${colXsClass} ${colSmClass} ${colMdClass} ${colLgClass} ${colXlClass} ${xsOffsetClass} ${smOffsetClass} ${mdOffsetClass} ${lgOffsetClass} ${xlOffsetClass}`.trim();
     ;
 
     return (
