5b8bc97758b1751826179d67e6d8b6323895d623
Luis Vinicius <luis@startamus.com.br>
2016-12-14 16:44:02 -0200

    fixing tabs component base when update component

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -18,6 +18,33 @@ function renderChildren(tab) {
  * @name Tabs
  */ 
 export default class Tabs extends Component {
+  componentDidUpdate(){
+      let initialTabActive = this.props.initialSelectedIndex || 0;
+      this.props.children.forEach(function (tab, i ){
+        // save all children of Tab Components in array
+        renderChildren(tab.children);
+        let value = document.querySelector(`#${tab.attributes.value}`);
+        value.innerHTML = _tabComponentsChildren[i];
+      });
+
+      //Tabs Component Children
+      let tabsVNode = this.props.children;
+
+      tabsVNode.forEach(function (tab, i) {
+        let div = document.createElement('div');
+        div.classList.add('mui-tabs__pane');
+        div.setAttribute('data-is', 'tab-mui');
+        div.id = tab.attributes.value;
+        if (i === initialTabActive) {
+          div.classList.add('mui--is-active');
+        }
+
+        div.innerHTML = _tabComponentsChildren[i];
+      let tabsMUI = document.querySelector('[data-is="tabs-mui"]');
+      tabsMUI.insertAdjacentElement('afterend', div);
+      });
+       _tabComponentsChildren = []
+  }
   componentDidMount () {
     // the initial Tab active index
     let initialTabActive = this.props.initialSelectedIndex || 0;
@@ -41,9 +68,11 @@ export default class Tabs extends Component {
       }
 
       div.innerHTML =  _tabComponentsChildren[i];
-
-      this.base.insertAdjacentElement('afterend', div);
+      this.base.setAttribute('data-is', 'tabs-mui');
+      let tabsMUI = document.querySelector('[data-is="tabs-mui"]');
+      tabsMUI.insertAdjacentElement('afterend', div);
     });
+    _tabComponentsChildren = []
   }
  render({children, justified, initialSelectedIndex, ...props}) {
   let className = justified ? 'mui-tabs__bar mui-tabs__bar--justified' : 'mui-tabs__bar';
