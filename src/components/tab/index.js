083c552cbc1015dc66de385dd84d08ecbf5db592
Luis Vinicius <luis@startamus.com.br>
2016-12-16 19:21:54 -0200

    refactoring tabs and tab components

--- src/components/tab/index.js
+++ src/components/tab/index.js
@@ -1,30 +1,40 @@
-/**
- * MUI Preact Tab Module
- * @module preact/tab
- */
+  /**
+   * MUI Preact Tab Module
+   * @module preact/tab
+   */
 
-'use strict';
+  'use strict';
 
-import {h, Component, render} from 'preact';
-/**
- * @name Tab
- */ 
-export default class Tab extends Component {
-  componentDidMount(){
-      let div = document.createElement('div');
-      div.classList.add('mui-tabs__pane');
-      div.id = this.props.value;
-      if ( this.props.selected === true ) {
-        div.classList.add('mui--is-active');
-      }
-      div.appendChild(render(this.props.children[0]))
-      this.base.parentElement.insertAdjacentElement('afterend', div);
+  import {h, Component} from 'preact';
+  import {render as renderer} from 'preact';
+  
+  function renderTabMuiChildren(props){
+    let div = document.createElement('div');
+        div.classList.add('mui-tabs__pane');
+        div.id = props.value;
+        if ( props.selected === true ) {
+          div.classList.add('mui--is-active');
+        }
+        div.appendChild(renderer(props.children[0]))
+        return div;
   }
- render({value, selected, label, ...props}) {
-   return (
-      <li {...props} class={selected ? 'mui--is-active' : null}>
-        <a data-mui-toggle="tab" data-mui-controls={value}> {label} </a>
-      </li>
-   )
- }
-}
+  /**
+   * @name Tab
+   */ 
+  export default class Tab extends Component {
+    componentDidUpdate(){
+        let div = renderTabMuiChildren(this.props);
+        this.base.parentElement.insertAdjacentElement('afterend', div);
+    }    
+    componentDidMount(){
+        let div = renderTabMuiChildren(this.props);
+        this.base.parentElement.insertAdjacentElement('afterend', div);
+    }
+  render({value, selected, label, ...props}) {
+    return (
+        <li {...props} class={selected ? 'mui--is-active' : null}>
+          <a data-mui-toggle="tab" data-mui-controls={value}> {label} </a>
+        </li>
+    )
+  }
+  }
